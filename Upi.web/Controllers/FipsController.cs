95fafa40c253b5a8827955455620b17dd6b5c268
Mark <mark@bessett.org>
2017-09-21 20:21:59 -0700

    ui & api fixups for usability

--- Upi.web/Controllers/FipsController.cs
+++ Upi.web/Controllers/FipsController.cs
@@ -24,28 +24,54 @@ namespace Upi.web.Controllers
         [HttpGet("{stateId}", Name = "GetCounties")]
         public IEnumerable<FipsCounty> Get(string stateId)
         {
-            return FipsCache.FIPS.States.FirstOrDefault(s=> s.Code==stateId)
-                                .Counties.Where(c=> c.StatusCode!="H5")
-                                .OrderBy(c=> c.Name)
-                                .Select(c=> new FipsCounty {Name= $"{c.Name} ({c.Code}) [{c.StatusCode}]", Code=c.Code, StatusCode=c.StatusCode })
-                                .ToList();
+            var counties = new List<FipsCounty>();
+            var fipsState = FipsCache.FIPS.States.FirstOrDefault(s => s.Code == stateId || s.Name.Equals(stateId, StringComparison.CurrentCultureIgnoreCase));
+
+            if (fipsState != null)
+            {
+                var fipsResults = fipsState
+                                .Counties.Where(c => c.StatusCode != "H5")
+                                .OrderBy(c => c.Name).ToList();
+
+                if (fipsResults != null)
+                {
+                    counties.AddRange( 
+                        fipsResults
+                            .Select(c => new FipsCounty { Name = $"{c.Name} ({c.Code}) [{c.StatusCode}]", Code = c.Code, StatusCode = c.StatusCode })
+                            .ToList());
+                }
+            }
+
+            return counties;
         }
 
         [HttpGet("{stateId}/{countyId}", Name = "GetSubCounties")]
         public IEnumerable<FipsSubCounty> Get(string stateId, string countyId)
         {
             var subcounties = new List<FipsSubCounty>();
-            subcounties.Add(new FipsSubCounty() { Code = "N", Name = "N/A", FunctionalStatus = "" });
+            var fipsState = FipsCache.FIPS.States.FirstOrDefault(s => s.Code == stateId || s.Name.Equals(stateId, StringComparison.CurrentCultureIgnoreCase));
 
-            subcounties.AddRange(
-                FipsCache.FIPS.States.FirstOrDefault(s => s.Code == stateId)
-                                .Counties.FirstOrDefault(c=> c.Code == countyId)
-                                .SubCounties.Where(s=> s.FunctionalStatus != "F" && s.FunctionalStatus != "N")
-                                .OrderBy(c => c.Name)
-                                .Select(s=> new FipsSubCounty { Name= $"{s.Name} ({s.Code}) [{s.FunctionalStatus}]", Code = s.Code, FunctionalStatus=s.FunctionalStatus })
-                                .ToList()
-                );
+            if (fipsState != null)
+            {
+                var fipsCounty = fipsState.Counties.FirstOrDefault(c => c.Code == countyId);
 
+                if (fipsCounty != null)
+                {
+                    var naOption = new FipsSubCounty() { Code = "N", Name = "N/A", FunctionalStatus = "" };
+
+                    subcounties.Add(naOption);
+                    var invalidFunctionalStatuses = "FN";
+
+                    var definedSubcounties = fipsCounty
+                                        .SubCounties.Where(s => !invalidFunctionalStatuses.Contains(s.FunctionalStatus))
+                                        .OrderBy(c => c.Name)
+                                        .Select(s => new FipsSubCounty { Name = $"{s.Name} ({s.Code}) [{s.FunctionalStatus}]", Code = s.Code, FunctionalStatus = s.FunctionalStatus })
+                                        .ToList();
+
+                    subcounties.AddRange(definedSubcounties);
+                }
+
+            }
             return subcounties;
         }
 
